9fffdd469705c647eaaeadf05c2f6a8c15fef616
Karanaproject <projectmailrana@gmail.com>
2025-09-28 21:17:10 +0700

    Update experiences.ts

--- app/data/experiences.ts
+++ app/data/experiences.ts
@@ -1,41 +1,60 @@
 const experiences = [
   {
-    company: "Woolworths Group",
-    role: "Frontend Developer",
-    year: "Jan 2025 - Present",
-    logo: "./logos/woolworths.webp",
+    company: "PT. FKS Pangan Nusantara",
+    role: "Sales Commander",
+    year: "May 2024 - Dec 2024",
+    logo: "./logos/fks.webp",
     size: "w-[40px] h-[40px]",
-    description: ["Contracted by Woolworths Group to work on a new project with React for frontend"],
-  },
-  {
-    company: "OpenOnion",
-    role: "Software Engineer Intern",
-    year: "Sep 2024 - Jan 2025",
-    logo: "./logos/openonion.webp",
-    size: "w-[40px] h-[40px]",
+    location: "Surabaya, Indonesia",
     description: [
-      "Maintained and debugged company’s main product (a startup), a community-focused AI chatbot built with Next.js for frontend, FastAPI for backend, MongoDB for the database, and Mistral AI for large language model (LLM) development",
-    ]
-  },
-  {
-    company: "Cloud NC",
-    role: "Software Engineer Intern",
-    year: "Sep 2023 - Feb 2024",
-    logo: "./logos/cloudnc.webp",
-    description: [
-      "Produced and maintained two Next.js websites using Express.js as server and PostgreSQL as database, tailored to customers’ requirements: a booking management platform and an e-commerce platform",
+      "Led 4 sales representatives through journey planning (PJP), field monitoring, and routine mentoring to improve visit effectiveness and achieve sales targets",
+      "Conducted accounts receivable monitoring through invoice audits, successfully increasing collection rate from 70% to 100%",
+      "Managed wholesale and industrial segment customers, contributing 25% of total branch sales revenue target",
+      "Achievement: Ranked 2nd National KPI Productivity (Revenue, Volume, Focus Products) (October 2024)"
     ],
-    size: "w-[40px] h-[40px]",
   },
   {
-    company: "UNSW Computer Sceience and Engineering",
-    role: "Casual Academic (Teaching Assistant)",
-    year: "May 2023 - Sep 2023",
-    logo: "./logos/unsw-cse.webp",
-    description: [
-      "Assessed and provided detailed feedback on 100+ student assignments in an Algorithm and Programming Techniques course (COMP3121) at the University of New South Wales",
-    ],
+    company: "PT. FKS Pangan Nusantara",
+    role: "Territory Sales Supervisor",
+    year: "Aug 2023 - May 2024",
+    logo: "./logos/fks.webp",
     size: "w-[40px] h-[40px]",
+    location: "Mojokerto, Indonesia",
+    description: [
+      "Led 3 sub-distributors across 6 regions (Jombang, Mojokerto, Lamongan, Tuban, Bojonegoro and West Surabaya), achieving 150% YTD sales growth (2023 vs 2024)",
+      "Developed a structured stock forecasting system to improve forecasting accuracy and reduce potential bad stock",
+      "Conducted distribution audits in central markets to drive New Outlet Opening (NOO) increase up to 20% per month and expand coverage area"
+    ],
+  },
+  {
+    company: "PT. Marga Nusantara Jaya",
+    role: "Area Sales Manager",
+    year: "May 2022 - Jul 2023",
+    logo: "./logos/marga.webp",
+    size: "w-[40px] h-[40px]",
+    location: "Multiple Regions (Lampung, Jambi, South Sulawesi)",
+    description: [
+      "Led over 10 sales representatives in GT, MT including LKA & NKA as well as Horeca and Institutional sectors",
+      "Prepared and evaluated annual distribution plans (Territory Management, Sales Target Allocation, Route-to-Market strategy) to improve distribution system effectiveness based on area potential",
+      "Managed MT Key Account sales, successfully achieving 100% absorption target for festive season products",
+      "Implemented operational sales system including accounts receivable audits and collection monitoring to reduce bad debt and minimize fraud",
+      "Executed BTL programs (trade promo, event activation, branding) and outlet programs with SPG support, impacting sales revenue and brand awareness",
+      "Achievement: Ranked 1st National KPI Productivity (Call, Effective Call, NOO Growth, Revenue, Focus Products)"
+    ],
+  },
+  {
+    company: "PT. Marga Nusantara Jaya",
+    role: "Management Trainee",
+    year: "Jan 2021 - May 2022",
+    logo: "./logos/marga.webp",
+    size: "w-[40px] h-[40px]",
+    location: "Multiple Regions (South Sulawesi, Central Sulawesi, South Kalimantan)",
+    description: [
+      "Conducted practice-based learning in distribution operations and promotional activities to comprehensively understand distribution processes and develop distribution system innovation projects",
+      "Led the Food Division GT zoning project by changing the canvas sales system to a taking order system, increasing sales from 15 million rupiah/year to 30 million rupiah/month",
+      "Made partnership proposals with institutions (South Sulawesi BPOM and Makassar City KPU) for multivitamin product procurement (Ester-C, Renovit, Protecal) to enhance business negotiation and customer management experience",
+      "Achievement: Scored 2.87/3.00 in the Management Trainee program, reflecting excellence in presentation quality, distribution concept understanding, leadership ability, and innovative thinking"
+    ],
   },
 ];
 
