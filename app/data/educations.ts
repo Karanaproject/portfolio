a360bfd60a1e6e6c4a7f0e404c11a2e344ba3896
Karanaproject <projectmailrana@gmail.com>
2025-09-28 21:13:59 +0700

    Update educations.ts

--- app/data/educations.ts
+++ app/data/educations.ts
@@ -1,35 +1,11 @@
 const educations = [
   {
-    company: "The University of New South Wales",
-    role: "Bachelor of Computer Engineering (Honours)",
-    year: "Feb 2022 - Dec 2024",
-    logo: "./logos/unsw.webp",
+    company: "Gadjah Mada University",
+    role: "Bachelor of Agriculture",
+    year: "Aug 2014 - Jul 2019",
+    logo: "./logos/ugm.webp",
     description: [
-      "Winner of the UNSW College Hackathon 2024",
-    ],
-  },
-  {
-    company: "Thammasat University",
-    role: "Bachelor of Electrical Engineering (TEP)",
-    year: "2019 - 2021",
-    logo: "./logos/tu.webp",
-    description: [
-    ],
-  },
-  {
-    company: "Satri Angthong School",
-    role: "High School Diploma",
-    year: "2016 - 2018",
-    logo: "./logos/satri.webp",
-    description: [
-    ],
-  },
-  {
-    company: "Vajiravudh College",
-    role: "Middle School Diploma",
-    year: "2013 - 2015",
-    logo: "./logos/vaji.webp",
-    description: [
+      "2nd Place Winner of Scientific Writing Competition at Jenderal Soedirman University, Central Java & DIY Province Level 2015"
     ],
   },
 ];
