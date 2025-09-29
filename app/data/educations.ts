4ed8e37bdf904c014944428b3e63548fd445acd9
Karanaproject <projectmailrana@gmail.com>
2025-09-29 22:20:21 +0700

    Update educations.ts

--- app/data/educations.ts
+++ app/data/educations.ts
@@ -3,7 +3,7 @@ const educations = [
     company: "Gadjah Mada University",
     role: "Bachelor of Agriculture",
     year: "Aug 2014 - Jul 2019",
-    logo: "./logos/ugm.webp",
+    logo: "./logos/UGM.webp",
     description: [
       "2nd Place Winner of Scientific Writing Competition at Jenderal Soedirman University, Central Java & DIY Province Level 2015"
     ],
