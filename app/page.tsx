494da4d575300fca47a3d13ac66da00faa2b8739
Karanaproject <projectmailrana@gmail.com>
2025-09-29 22:30:04 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,24 @@ import HeroSection from "@/app/components/sections/HeroSection";
 import ProjectSection from "@/app/components/sections/ProjectSection";
 import AboutSection from "@/app/components/sections/AboutSection";
 import ContactSection from "@/app/components/sections/ContactSection";
+import ExperienceSection from "@/app/components/sections/ExperienceSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen relative">
+      {/* Hero/Banner utama */}
       <HeroSection />
+      
+      {/* Tentang saya */}
       <AboutSection />
+      
+      {/* Ringkasan pengalaman kerja */}
+      <ExperienceSection />
+      
+      {/* Proyek-proyek */}
       <ProjectSection />
+      
+      {/* Kontak */}
       <ContactSection />
     </main>
   );
