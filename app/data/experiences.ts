0bf75205bab9490dd2a4d24323011f05582a794b
Karanaproject <projectmailrana@gmail.com>
2025-09-29 22:51:48 +0700

    Update experiences.ts

--- app/data/experiences.ts
+++ app/data/experiences.ts
@@ -30,7 +30,7 @@ const experiences = [
     company: "PT. Marga Nusantara Jaya",
     role: "Area Sales Manager",
     year: "May 2022 - Jul 2023",
-    logo: "./logos/Marga.webp",
+    logo: "./logos/logo mnj.webp",
     size: "w-[40px] h-[40px]",
     location: "Multiple Regions (Lampung, Jambi, South Sulawesi)",
     description: [
@@ -46,7 +46,7 @@ const experiences = [
     company: "PT. Marga Nusantara Jaya",
     role: "Management Trainee",
     year: "Jan 2021 - May 2022",
-    logo: "./logos/Marga.webp",
+    logo: "./logos/logo.mnj",
     size: "w-[40px] h-[40px]",
     location: "Multiple Regions (South Sulawesi, Central Sulawesi, South Kalimantan)",
     description: [
