50e9e70f51ecb84ae67f1fb32371e2b37c32c1c4
Karanaproject <projectmailrana@gmail.com>
2025-09-29 23:16:07 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroSection from "@/app/components/sections/HeroSection";
 import ProjectSection from "@/app/components/sections/ProjectSection";
 import AboutSection from "@/app/components/sections/AboutSection";
 import ContactSection from "@/app/components/sections/ContactSection";
-// Hapus baris import ExperienceSection
+
 
 export default function Home() {
   return (
